eebdb480c5719687a0d82f5e5ca1559fec68d4b7
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2024-09-30 09:06:33 +0530

    Create a Keylog User Control Page

---
+++ KeyManagementSystem/Keylog.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace KeyManagementSystem
+{
+    partial class Keylog
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(22, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 19);
+            label1.TabIndex = 0;
+            label1.Text = "History";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(22, 51);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(832, 478);
+            dataGridView1.TabIndex = 1;
+            // 
+            // Keylog
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(dataGridView1);
+            Controls.Add(label1);
+            Name = "Keylog";
+            Size = new Size(875, 565);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private DataGridView dataGridView1;
+    }
+}
